a7e3f44200f50a0019b5dd1843bd7c725e2aba00
flogrammer <FlorianRottach@aol.com>
2016-10-29 16:18:18 +0200

    Testcommit

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,11 +1,13 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -19,23 +21,35 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
+    /**
+     * Die Methode simpleInitApp beeinhaltet alle Elemente die  
+     * anfangs geladen werden und sich nicht bewegen. (Modelle, Texturen, Bilder...)
+     */
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        Spatial figure = assetManager.loadModel("Models/Oto/Oto.mesh.xml");
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        figure.scale(0.6f); // Set Size
+    
+        DirectionalLight light = new DirectionalLight();
+        light.setDirection(new Vector3f(-1.0f,-1.0f,-1.0f));
+        
+        rootNode.attachChild(figure);
+        rootNode.addLight(light);
     }
-
+    /**
+     * Alle Elemente die sich verändern werden hier neu upgedated und an das Spiel angepasst
+     * @param tpf ist die time per frame 
+     * Damit das Spiel überall gleich schnell abläuft wird diese Größe benötigt
+     */
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
     }
-
+    /**
+     * Rendering von Texturen / Modellen und co
+     * @param rm 
+     */
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
